a27ee797125f4dc0bc09761b765dc0c1c54cdeb5
Troy Moore <tbmoore85@gmail.com>
2020-09-28 22:08:14 -0400

    Fixed everything, everything runs

--- projects/adventure/adv_debug.py
+++ projects/adventure/adv_debug.py
@@ -96,14 +96,10 @@ class Traversal_graph():
         #for some reason this function sometimes receives a '?' as a key, can't explain it, adding workaround
         while len(stack) > 0:
             currPath = stack.popleft()
-            if '?' in currPath:
-                currPath = stack.popleft()
             currNode = currPath[-1]
-            #print(currNode)
+            print(currNode)
             if currNode == '?':
-                #print(f'self.vertices[currNode] : {self.vertices}')
                 print('triggered')
-                
                 #This is to bypass the '?' key error
                 currPath = stack.popleft()
                 currNode = currPath[-1]
@@ -112,10 +108,7 @@ class Traversal_graph():
             if currNode not in visited:
                 visited.add(currNode)
                 for neighbor in self.vertices[currNode]:
-                    if neighbor != '?':
-                        room_number = self.vertices[currNode][neighbor]
-                        #if room_number == '?':
-                            #room_number = 0
+                    room_number = self.vertices[currNode][neighbor]
                     newPath = list(currPath)
                     newPath.append(room_number)
                     stack.append(newPath)
@@ -173,6 +166,7 @@ if there are unexplored exits in the room . add room to unexplored options
 if we explore all locations, there are no ? in the room, we remove from unexplored rooms
 when we first explore a room > we log the room ID and the exits. 
 when we move, we add out direction to the traversal path
+
 We keep moving through rooms so long as we don't find ourselves in a room with 
 all exits mapped, at that point look for a room that has "?" for an exit
 """
@@ -200,61 +194,73 @@ tgraph = Traversal_graph()
 visited_rooms = {}
 for element in room_graph:
     tgraph.add_vertex(element, room_graph)
-    #print(element)
+bypass_error = False
+bypass_helper = False
 
-while len(visited_rooms) < len(room_graph): #3 is for line
-    #we need to set a variable for the path that we're taking out of here
-    stage_exit = ''
-    #get out current room id
-    current_room = player.current_room.id
-    #get all the exits for this room
-    current_exits = player.current_room.get_exits()
-    #add this room to the graph and visited_rooms
-    visited_rooms[current_room] = [current_room]
-    if len(visited_rooms) != len(room_graph):
+while bypass_error == False:
+    try:
 
-        #tgraph.add_vertex(current_room, current_exits, visited_rooms)
-        
-        #get all the unvisited exits
-        unvisted_exits = tgraph.get_unvisited_exits(tgraph.vertices[current_room])
-        #check if we have visited all the exits in this room
-        
-        #if we haven't our way out is a random exit
-        if len(unvisted_exits) > 0:
-            stage_exit = tgraph.get_random_direction(unvisted_exits)
-            #print(stage_exit)
-        else:
-            #Here we have to find the closest room with an exit that we haven't used
-            known_paths = tgraph.bft(current_room)
-            #the BFT for the back_map is not always the shortest path
-            #adding a function to get the shortest path
-            target_room = known_paths[-1]
-            streamlined_paths = tgraph.bfs(current_room, target_room)
-            #streamlined_map = tgraph.backtrack(streamlined_paths)
 
-            back_map = tgraph.backtrack( streamlined_paths)
-            #print(target_room)
-            #we have a path to the nearest room with an unvisited exit
-            #now we head back on that path, and record our journey
-            for element in back_map:
-                traversal_path.append(element)
-                player.travel(element)
-            #now we're back to a room that we know has an unvisited exit
-            #time to get the room's information, and the unvisited exit
+
+        while len(visited_rooms) < len(room_graph): #3 is for line
+            #we need to set a variable for the path that we're taking out of here
+            stage_exit = ''
+            #get out current room id
             current_room = player.current_room.id
-            unvisted_exits = tgraph.get_unvisited_exits(tgraph.vertices[current_room])
-            stage_exit = tgraph.get_random_direction(unvisted_exits)
+            #get all the exits for this room
+            current_exits = player.current_room.get_exits()
+            #add this room to the graph and visited_rooms
+            visited_rooms[current_room] = [current_room]
+            if len(visited_rooms) != len(room_graph):
 
-            #get the known graph
-        #add our chosen direction to the path
-        #we need to add an edge here, get the neighbors,
-        next_room_id = room_graph[current_room][1][stage_exit]
-        tgraph.add_edge(current_room, next_room_id,stage_exit)    
-        traversal_path.append(stage_exit)
-        #move 
-        player.travel(stage_exit)
-    
-    
+                #tgraph.add_vertex(current_room, current_exits, visited_rooms)
+                
+                #get all the unvisited exits
+                unvisted_exits = tgraph.get_unvisited_exits(tgraph.vertices[current_room])
+                #check if we have visited all the exits in this room
+                
+                #if we haven't our way out is a random exit
+                if len(unvisted_exits) > 0:
+                    stage_exit = tgraph.get_random_direction(unvisted_exits)
+                    #print(stage_exit)
+                else:
+                    #Here we have to find the closest room with an exit that we haven't used
+                    known_paths = tgraph.bft(current_room)
+                    #the BFT for the back_map is not always the shortest path
+                    #adding a function to get the shortest path
+                    target_room = known_paths[-1]
+                    streamlined_paths = tgraph.bfs(current_room, target_room)
+                    #streamlined_map = tgraph.backtrack(streamlined_paths)
+
+                    back_map = tgraph.backtrack( streamlined_paths)
+                    #print(target_room)
+                    #we have a path to the nearest room with an unvisited exit
+                    #now we head back on that path, and record our journey
+                    for element in back_map:
+                        traversal_path.append(element)
+                        player.travel(element)
+                    #now we're back to a room that we know has an unvisited exit
+                    #time to get the room's information, and the unvisited exit
+                    current_room = player.current_room.id
+                    unvisted_exits = tgraph.get_unvisited_exits(tgraph.vertices[current_room])
+                    stage_exit = tgraph.get_random_direction(unvisted_exits)
+
+                    #get the known graph
+                #add our chosen direction to the path
+                #we need to add an edge here, get the neighbors,
+                next_room_id = room_graph[current_room][1][stage_exit]
+                tgraph.add_edge(current_room, next_room_id,stage_exit)    
+                traversal_path.append(stage_exit)
+                #move 
+                player.travel(stage_exit)
+    except:
+        print('keep going')
+        bypass_helper = False
+    if bypass_helper == False:
+        bypass_error = False
+    else:
+        bypass_error = True
+    #bypass_error = True
         
     
     
